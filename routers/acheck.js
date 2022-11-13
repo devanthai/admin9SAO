9409c05194c874cf9fb08e250afd1e63d3ca8399
devanthai <thaile2462@gmail.com>
2022-11-13 07:54:16 +0700

    dfg

--- routers/acheck.js
+++ routers/acheck.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const TaiXiu = require("../models/taixiu/Cuoc")
 const ChanLe = require("../models/chanle/Cuoc")
+const TaiXiu2 = require("../models/taixiu/Lichsu")
+const ChanLe2 = require("../models/chanle/Lichsu")
 const Cuoc = require("../models/Cuoc")
 
 router.get("/taixiu", async (req, res) => {
@@ -14,6 +16,21 @@ router.get("/chanle", async (req, res) => {
     let now = new Date();
     let DATE = new Date(now.getFullYear(), now.getMonth(), now.getDate() - (req.query.day || 0));
     const taixius = await ChanLe.find({ time: { $gte: DATE } }).sort({ time: -1 })
+    res.render("checkcan/chanle", { taixius })
+})
+
+
+router.get("/taixiu2", async (req, res) => {
+    let now = new Date();
+    let DATE = new Date(now.getFullYear(), now.getMonth(), now.getDate() - (req.query.day || 0));
+    const taixius = await TaiXiu2.find({ time: { $gte: DATE } }).sort({ time: -1 })
+    res.render("checkcan/taixiu", { taixius })
+})
+
+router.get("/chanle2", async (req, res) => {
+    let now = new Date();
+    let DATE = new Date(now.getFullYear(), now.getMonth(), now.getDate() - (req.query.day || 0));
+    const taixius = await ChanLe2.find({ time: { $gte: DATE } }).sort({ time: -1 })
     res.render("checkcan/chanle", { taixius })
 })
 
